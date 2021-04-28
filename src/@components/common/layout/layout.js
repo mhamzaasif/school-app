e12cac1df58766ecc90b4aae24b3dc03ba35cbc6
Muhammad Hamza <muhammad.hamza@phaedrasolutions.com>
2021-04-28 17:06:08 +0500

    optimized sidebar

--- src/@components/common/layout/layout.js
+++ src/@components/common/layout/layout.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { Container, Navbar, Nav, Button } from "react-bootstrap";
 import Sidebar from "../sidebar/sidebar";
@@ -7,20 +7,27 @@ import { rootContext } from "../../../@context";
 
 function Layout({ component: Component }) {
 	const { logoutUser } = useContext(rootContext);
+	const [toggleSidebar, setToggleSidebar] = useState(false);
 	return (
 		<Container fluid>
 			<Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+				<Navbar.Toggle
+					aria-controls="responsive-navbar-nav"
+					onClick={() => setToggleSidebar((prevState) => !prevState)}
+				/>
+				<div
+					className={`${toggleSidebar ? "responsive-sidebar px-3" : "d-none"}`}
+				>
+					<Sidebar />
+				</div>
 				<Navbar.Brand as={Link} to="/">
 					School ABC
 				</Navbar.Brand>
-				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-				<Navbar.Collapse id="responsive-navbar-nav">
-					<Nav className="ml-auto">
-						<Nav.Link as={Button} onClick={() => logoutUser()}>
-							Logout
-						</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
+				<Nav className="ml-auto">
+					<Nav.Link as={Button} onClick={() => logoutUser()}>
+						Logout
+					</Nav.Link>
+				</Nav>
 			</Navbar>
 			<div className="container-layout">
 				<div className="d-none d-lg-block">
